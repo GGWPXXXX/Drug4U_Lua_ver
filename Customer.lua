0e1523d3486c19220924c27d47bce0df5ba7c68d
Pattadon Loyprasert <pattadon2546pn@gmail.com>
2022-12-22 22:02:49 +0700

    Construting login.

--- Customer.lua
+++ Customer.lua
@@ -5,6 +5,47 @@ function login()
     print('Hi welcome to login page')
     print('Please type in your username and password :)')
     print('============================')
-    print("Your username?")
+  
+  
+    -- Open the JSON file
+    local file, err = io.open("../Drug4U_Lua_ver/User_file/User_data.json", 'r')
+    if not file then
+      print("Error opening file: " .. err)
+      return
+    end
+  
+    -- Read the contents of the file into a string
+    local data = file:read("*all")
+    file:close()
+  
+    -- Parse the JSON string into a Lua table
+    local customer_data = lunajson.decode(data)
+    if not customer_data then
+      print("Error parsing JSON data: " .. err)
+      return
+    end
 
+    print("Your username?")
+    local username = io.read()
+    while True do
+        print("True")
+        found = nil
+        for username_in_db in pairs(customer_data) do
+            if username_in_db == username then 
+              found = True
+            end
+        end
+    
+        if found then
+            break
+        end
+    
+        if found == False then
+            print("You're not in the system.")
+            print("Your username?")
+            username = io.read()
+        end
+    end
 end
+
+login()
