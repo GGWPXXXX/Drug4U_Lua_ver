fac180d2a91885331c6f268a943f2e790f0d0549
Pattadon Loyprasert <pattadon2546pn@gmail.com>
2022-12-22 18:39:10 +0700

    Initial commit.

---
+++ Customer.lua
@@ -0,0 +1,10 @@
+local lunajson = require("lunajson")
+
+function login()
+    print('============================')
+    print('Hi welcome to login page')
+    print('Please type in your username and password :)')
+    print('============================')
+    print("Your username?")
+
+end
---
+++ main.lua
@@ -0,0 +1,15 @@
+print("===========================")
+print("Welcome to My DRUG4U Shop!")
+print("===========================")
+print('Are you a customer?')
+
+---Check that user is a customer or admin
+print("(y/n): ")
+local check_wheter_customer = io.read()
+
+---Check customer's input
+while check_wheter_customer:lower() ~= 'y' and check_wheter_customer:lower() ~= 'n' do
+    print('Please type (y/n)')
+    check_wheter_customer = io.read()
+end
+
